114120ae29997bb39c049621b829e928ade6179a
Petterson Neves <pettersonkesler@gmail.com>
2024-12-25 13:58:41 -0300

    aplicando o pattern matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -7,7 +7,13 @@ defmodule Servy.Handler do
   end
 
   def parse(request) do
-    conv = %{method: "GET", path: "wildthigs", resp_body: ""}
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
