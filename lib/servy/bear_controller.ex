e7f0d4ecb58d62cdcdd55fc1e09037e0dbeed7f5
Petterson Neves <pettersonkesler@gmail.com>
2025-06-24 02:44:24 -0300

    16 - refactor duplicate code to a render function

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -4,23 +4,24 @@ defmodule Servy.BearController do
 
   @template_path Path.expand("../../templates", __DIR__)
 
+  defp render(conv, template, bindings \\ []) do
+    content = @template_path |> Path.join(template) |> EEx.eval_file(bindings)
+    %{conv | status: 200, resp_body: content}
+  end
+
   def index(conv) do
     bears =
       Wildthings.list_bears()
       |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
 
-    content = @template_path |> Path.join("index.eex") |> EEx.eval_file(bears: bears)
-
-    %{conv | status: 200, resp_body: content}
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    content = @template_path |> Path.join("show.eex") |> EEx.eval_file(bear: bear)
-
-    %{conv | status: 200, resp_body: content}
+    render(conv, "show.eex", bear: bear)
   end
 
   def create(conv, %{"type" => type, "name" => name}) do
